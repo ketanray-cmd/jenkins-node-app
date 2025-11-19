713c9425142ef87c5489df93e1ec7392d13dc0d7
ketanray-cmd <ketan.ray@rmoneyindia.com>
2025-11-19 13:00:10 +0530

    Testing Webhooks

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 
+const app = express();
+
 app.use(express.static("public"));
 
-const app = express();
+
 
 // serve public folder
 app.use(express.static(path.join(__dirname, "public")));
